5dad4147c752c6f243658bc8ee6ba9e0f6a92b85
Iain Saxon <iain.saxon@newism.com.au>
2012-03-14 11:36:25 +1100

    cleaned up syntax and added print_and_exit tag for developers

--- mod.nsm_web_profiler.php
+++ mod.nsm_web_profiler.php
@@ -12,7 +12,8 @@
  * @see				http://expressionengine.com/public_beta/docs/development/modules.html#control_panel_file
  */
 
-class Nsm_web_profiler {
+class Nsm_web_profiler
+{
 
 	/**
 	 * PHP5 constructor function.
@@ -29,41 +30,54 @@ class Nsm_web_profiler {
 		$EE =& get_instance();
 
 		// define a constant for the current site_id rather than calling $PREFS->ini() all the time
-		if (defined('SITE_ID') == FALSE)
+		if (defined('SITE_ID') == FALSE) {
 			define('SITE_ID', $EE->config->item('site_id'));
+		}
 
 		// Init the cache
 		// If the cache doesn't exist create it
-		if (! isset($EE->session->cache[$this->addon_id]))
+		if (! isset($EE->session->cache[$this->addon_id])) {
 			$EE->session->cache[$this->addon_id] = array();
+		}
 
-		// Assig the cache to a local class variable
+		// Assign the cache to a local class variable
 		$this->cache =& $EE->session->cache[$this->addon_id];
 	}
 
-	public function toolbar() {
+	public function toolbar()
+	{
 		$EE =& get_instance();
-		if($EE->session->userdata['group_id'] != 1)
+		if ($EE->session->userdata['group_id'] != 1) {
 			return false;
+		}
 			
-		if(APP_VER < '2.1.5') {
+		if (APP_VER < '2.1.5') {
 			// EE < .2.2.0
 			return $EE->load->_ci_load(array(
 				'_ci_vars' => array(),
 				'_ci_path' => PATH_THIRD . $this->addon_id . '/views/module/toolbar.php',
 				'_ci_return' => true
 			));
-		}else{
+		} else {
 			$EE->load->add_package_path(PATH_THIRD . 'nsm_web_profiler');
 			return $EE->load->view('module/toolbar', array(), TRUE);
 		}
 	}
 	
-	public function disable_ee_debugging() {
+	public function disable_ee_debugging()
+	{
 		$EE =& get_instance();
 		$EE->output->enable_profiler(false);
 		$EE->TMPL->debugging = false;
 		return '';
+	}
+	
+	public function print_and_exit()
+	{
+		$EE =& get_instance();
+		$this->disable_ee_debugging();
+		$tagdata = trim($EE->TMPL->tagdata);
+		die($tagdata);
 	}
 	
 }
